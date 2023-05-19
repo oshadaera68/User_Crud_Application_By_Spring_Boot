4428bdace99db5acadbb8f5cd1e3bb3647dad5d3
oshadaera68 <mindartlk010@gmail.com>
2023-05-20 00:40:27 +0530

    added annotations and methods in project

--- src/main/java/com/eraboy/usercrud/config/WebAppConfig.java
+++ src/main/java/com/eraboy/usercrud/config/WebAppConfig.java
@@ -1,4 +1,13 @@
 package com.eraboy.usercrud.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class WebAppConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
--- src/main/java/com/eraboy/usercrud/controller/UserController.java
+++ src/main/java/com/eraboy/usercrud/controller/UserController.java
@@ -1,4 +1,9 @@
 package com.eraboy.usercrud.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
 }
